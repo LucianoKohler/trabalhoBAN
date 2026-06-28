fa814c085158ef6d99ae937f10e0258697f42853
LuizaMannes <lulusil23@gmail.com>
2026-06-28 17:02:02 -0300

    produto

--- sistemacafeteria/src/main/java/com/cafeteria/SQL/ConexaoDB.java
+++ sistemacafeteria/src/main/java/com/cafeteria/SQL/ConexaoDB.java
@@ -11,20 +11,13 @@ public class ConexaoDB {
     private ConexaoDB() {}
 
     public static Connection getInstancia() {
-<<<<<<< HEAD:sistemacafeteria/src/main/java/com/cafeteria/SQL/ConexaoDB.java
-        try {
-            if (conexao == null || conexao.isClosed()) {
-=======
         if (conexao == null) {
             /* 
             try {
->>>>>>> 8ec63ae7255112a7488d74ec46c0f5e0a91a8395:sistemacafeteria/src/main/java/com/cafeteria/banco/ConexaoDB.java
                 conexao = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5433/cafeteria",
                     "postgres", "1234"
                 );
-<<<<<<< HEAD:sistemacafeteria/src/main/java/com/cafeteria/SQL/ConexaoDB.java
-=======
             } catch (SQLException e) {
                 System.out.println("Erro ao conectar: " + e.getMessage());
             }*/
@@ -35,11 +28,8 @@ public class ConexaoDB {
                 );
             } catch (SQLException e) {
                 System.out.println("Erro ao conectar: " + e.getMessage());
->>>>>>> 8ec63ae7255112a7488d74ec46c0f5e0a91a8395:sistemacafeteria/src/main/java/com/cafeteria/banco/ConexaoDB.java
             }
-        } catch (SQLException e) {
-            System.out.println("Erro ao conectar ou restabelecer conexão: " + e.getMessage());
-        }
+        } 
         return conexao;
     }
 }
